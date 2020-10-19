05a110c5edb0e10e15fc3526acf2210e3c751219
leposamarko <leposa.marko.daniel@gmail.com>
2020-10-19 15:07:55 +0200

    Project, layers, nugets created

---
+++ oenik_prog3_2020_2_z3vjc0/oenik_prog3_2020_2_z3vjc0/KutyaVerseny.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="KutyaVerseny.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace KutyaVerseny.Program
+{
+    /// <summary>
+    /// A program.
+    /// </summary>
+    public class KutyaVerseny
+    {
+        /// <summary>
+        /// Gets or sets Ez azért kell, hogy ne figyelmeztessen a StyleCop.
+        /// </summary>
+        public int Valami { get; set; }
+
+        /// <summary>
+        /// Itt kezdődik a program.
+        /// </summary>
+        public static void Main()
+        {
+        }
+    }
+}
